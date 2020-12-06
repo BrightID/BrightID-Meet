1f88338d815989c8ef9ad607bc780da9ba0b5ab2
Mohammadreza Rastegari <rastegari1375@gmail.com>
2020-12-06 16:25:31 +0330

    add footer

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,11 @@ import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
+
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+  },
     theme: {
         themes: {
           darck: {
